e47797be78e2e1f7b0812dd33177451f13c221b5
Jordan Kay <jordantkay21@gmail.com>
2022-09-01 21:03:32 -0400

    added player movement | User Input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //4 requirments of a variable
-        //1. public or private reference
-            //public = outside world can communicate with this variable (other scripts/game objects)
-            //private= ONLY the player object can communicate with this variable
-        //2. data type (int, float, bool, string)
-            //int = integer (whole numbers)
-            //float = decimal values
-            //bool = boolean (true/false)
-            //string = character of text
-        //3. every variable has a name
-        //4.optional value assigned
-    public float speed = 3.5f;
+    
+        [SerializeField] //Lets designers modify values within the Unity Inspector when variables are private
+    private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +14,29 @@ public class Player : MonoBehaviour
            
         //take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
+
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-            //new vector3(1, 0, 0) | Time.deltaTime = 1second
-            //new vector3(1, 0, 0) * 5 * real time (per second)
-                //above is the same as new vector3(5, 0, 0) * Time.deltaTime
+    {        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        //new vector3(1, 0, 0) | Time.deltaTime = 1second
+        //new vector3(1, 0, 0) * 5 * real time (per second)
+        //above is the same as new vector3(5, 0, 0) * Time.deltaTime
         //transform.Translate(Vector3.right * 5 * Time.deltaTime);
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        //new Vector3([-1 "left" or 1 "right"], 0, 0) * [-1 "left" or 0 "stay still" or "right"] * 3.5f * real time (1 second)
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 }
