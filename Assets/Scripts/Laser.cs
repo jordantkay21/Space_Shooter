b858af121c0eca3a9ddb272f21a8b8f467f3ae6f
Jordan Kay <jordantkay21@gmail.com>
2022-09-03 01:58:40 -0400

    Laser Movement | Destroy Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+    //speed variable of 8
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translate movement up
+
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //if laser position is greater than 8 on y
+        //then destroy the object
+
+        if (transform.position.y >= 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
