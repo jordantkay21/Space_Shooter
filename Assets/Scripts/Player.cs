dd40bcc5bfb17ab5d3ba81797ab3006c2e8fa553
Jordan Kay <jordantkay21@gmail.com>
2022-09-07 17:57:59 -0400

    Added Spawn Manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private int _lives = 3;
 
+    private SpawnManager _spawnManager;
+
 
 
     // Start is called before the first frame update
@@ -29,6 +31,12 @@ public class Player : MonoBehaviour
            
 
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL.");
+        }
 
         
     }
@@ -80,6 +88,7 @@ public class Player : MonoBehaviour
 
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
